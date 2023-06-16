8bbc3c5db87416bc48729b6387c7a9ec767e230e
isognomon007 <136846012+isognomon007@users.noreply.github.com>
2023-06-16 19:31:47 -0400

    added shake effect and changed paths to css/js

---
+++ js/effects.js
@@ -0,0 +1,6 @@
+// shake effect on index page
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    container.classList.add('shake');
+  });
+  
