9f7651193a0fe3e3941fcf73f77d91da0799dc33
isognomon007 <136846012+isognomon007@users.noreply.github.com>
2023-06-18 01:04:21 -0400

    shift bg

--- js/effects.js
+++ js/effects.js
@@ -4,3 +4,31 @@ window.addEventListener('DOMContentLoaded', () => {
   container.classList.add('shake');
   });
 
+  function shiftBackground(direction) {
+   var body = document.body;
+   var currentPosition = body.style.backgroundPosition;
+   var positionX = 0; // Initial X position
+   var positionY = 0; // Initial Y position
+ 
+   // Parse the current position values
+   var matches = currentPosition.match(/(-?\d+)px (-?\d+)px/);
+   if (matches && matches.length === 3) {
+     positionX = parseInt(matches[1]);
+     positionY = parseInt(matches[2]);
+   }
+ 
+   // Adjust the position based on the direction
+   if (direction === 'up') {
+     positionY -= 10; // Shift up by 10 pixels
+   } else if (direction === 'down') {
+     positionY += 10; // Shift down by 10 pixels
+   } else if (direction === 'left') {
+     positionX -= 10; // Shift left by 10 pixels
+   } else if (direction === 'right') {
+     positionX += 10; // Shift right by 10 pixels
+   }
+ 
+   // Set the new background position
+   body.style.backgroundPosition = positionX + 'px ' + positionY + 'px';
+ }
+ 
