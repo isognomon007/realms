03541d3d7b73256e0fead9d4d6906da244c98ad1
isognomon007 <136846012+isognomon007@users.noreply.github.com>
2023-06-29 09:51:30 -0400

    h

--- js/script.js
+++ js/script.js
@@ -40,66 +40,6 @@ buttonTags.forEach(function(buttonTag) {
 //////////////////////////////  NAVIGATION  ////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
-// Function to create a new backpack item
-function createBackpackItem(imageSrc, overlayText) {
-  var backpackItem = document.createElement('div');
-  backpackItem.classList.add('backpack-item');
-  
-  var imageLink = document.createElement('a');
-  imageLink.href = '#';
-  imageLink.addEventListener('click', function() {
-    toggleOverlay(overlayText);
-  });
-  
-  var image = document.createElement('img');
-  image.src = imageSrc;
-  image.alt = 'Backpack Item';
-  
-  imageLink.appendChild(image);
-  backpackItem.appendChild(imageLink);
-  
-  return backpackItem;
-}
-
-// Function to toggle the overlay
-function toggleOverlay(overlayText) {
-  var overlay = document.getElementById('overlay');
-  
-  if (overlay) {
-    overlay.remove();
-  } else {
-    overlay = document.createElement('div');
-    overlay.id = 'overlay';
-    
-    var overlayContent = document.createElement('div');
-    overlayContent.classList.add('overlay-content');
-    
-    var overlayTextElement = document.createElement('p');
-    overlayTextElement.textContent = overlayText;
-    
-    overlayContent.appendChild(overlayTextElement);
-    overlay.appendChild(overlayContent);
-    
-    overlay.addEventListener('click', function() {
-      overlay.remove();
-    });
-    
-    document.body.appendChild(overlay);
-  }
-}
-
-// Append the backpack items to the #nav element within the #content element
-var contentElement = document.getElementById('content');
-var navElement = document.createElement('div');
-navElement.id = 'nav';
-
-var backpackItem1 = createBackpackItem('https://lh3.google.com/u/6/d/1BkvSarSc1XXyjkpFEBBBSqy2MrbW7kIF=w2682-h1224-iv1', 'This is your backpack item 1.');
-var backpackItem2 = createBackpackItem('https://lh3.google.com/u/6/d/1BkvSarSc1XXyjkpFEBBBSqy2MrbW7kIF=w2682-h1224-iv1', 'This is your backpack item 2.');
-
-navElement.appendChild(backpackItem1);
-navElement.appendChild(backpackItem2);
-contentElement.appendChild(navElement);
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
